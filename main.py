41e110f6537787055604204e7acb691f0bd0b4b4
HamzaPrograms <158636090+HamzaPrograms@users.noreply.github.com>
2025-06-27 04:59:18 -0400

    Made AWS keys private

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import boto3
+from dotenv import load_dotenv
+
+load_dotenv()
+AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+AWS_REGION = os.getenv('AWS_REGION')
+
+s3 = boto3.client('s3',
+                  aws_access_key_id='AWS_ACCESS_KEY',
+                  aws_secret_access_key='AWS_SECRET_KEY',
+                  region_name='AWS_REGION')
+
